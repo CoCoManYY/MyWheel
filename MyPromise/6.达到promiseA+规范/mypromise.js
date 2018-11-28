76cab529e39f02df49b503f7d54e6d748d987f16
CoCoManYY <1344483030@qq.com>
2018-11-28 21:53:29 +0800

    支持promise/A+规范

---
+++ MyPromise/6.达到promiseA+规范/mypromise.js
@@ -0,0 +1,143 @@
+const PENDING = "pending";
+const FULFILLED = "fulfilled";
+const REJECTED = "rejected";
+
+function MyPromise(fn) {
+    const self = this;
+    self.value = null;
+    self.error = null;
+    self.status = PENDING;
+    self.onFulfilledCallbacks = [];
+    self.onRejectedCallbacks = [];
+
+    function resolve(value) {
+        if (value instanceof MyPromise) {
+            return value.then(resolve, reject);
+        }
+        if (self.status === PENDING) {
+            setTimeout(() => {
+                self.status = FULFILLED;
+                self.value = value;
+                self.onFulfilledCallbacks.forEach((callback) => callback(self.value));
+            }, 0)
+        }
+    }
+
+    function reject(error) {
+        if (self.status === PENDING) {
+            setTimeout(function() {
+                self.status = REJECTED;
+                self.error = error;
+                self.onRejectedCallbacks.forEach((callback) => callback(self.error));
+            }, 0)
+        }
+    }
+    try {
+        fn(resolve, reject);
+    } catch (e) {
+        reject(e);
+    }
+}
+
+function resolvePromise(bridgepromise, x, resolve, reject) {
+    //避免自己引用自己
+    if (bridgepromise === x) {
+        return reject(new TypeError('Circular reference'));
+    }
+    let called = false;
+    if (x != null && ((typeof x === 'object') || (typeof x === 'function'))) {
+        try {
+            // 是否是thenable对象（具有then方法的对象/函数）
+            //将 then 赋为 x.then
+            let then = x.then;
+            if (typeof then === 'function') {
+            //如果 then 是一个函数，以x为this调用then函数，且第一个参数是resolvePromise，第二个参数是rejectPromise
+                then.call(x, y => {
+                    if (called) return;
+                    called = true;
+                    resolvePromise(bridgepromise, y, resolve, reject);
+                }, error => {
+                    if (called) return;
+                    called = true;
+                    reject(error);
+                })
+            } else {
+            //如果 then不是一个函数，则 以x为值fulfill promise。
+                resolve(x);
+            }
+        } catch (e) {
+        //如果在取x.then值时抛出了异常，则以这个异常做为原因将promise拒绝。
+            if (called) return;
+            called = true;
+            reject(e);
+        }
+    } else {
+        resolve(x);
+    }
+}
+
+MyPromise.prototype.then = function(onFulfilled, onRejected) {
+    const self = this;
+    let bridgePromise;
+    onFulfilled = typeof onFulfilled === "function" ? onFulfilled : value => value;
+    onRejected = typeof onRejected === "function" ? onRejected : error => { throw error };
+    if (self.status === FULFILLED) {
+        return bridgePromise = new MyPromise((resolve, reject) => {
+            setTimeout(() => {
+                try {
+                    let x = onFulfilled(self.value);
+                    resolvePromise(bridgePromise, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            }, 0);
+        })
+    }
+    if (self.status === REJECTED) {
+        return bridgePromise = new MyPromise((resolve, reject) => {
+            setTimeout(() => {
+                try {
+                    let x = onRejected(self.error);
+                    resolvePromise(bridgePromise, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            }, 0);
+        });
+    }
+    if (self.status === PENDING) {
+        return bridgePromise = new MyPromise((resolve, reject) => {
+            self.onFulfilledCallbacks.push((value) => {
+                try {
+                    let x = onFulfilled(value);
+                    resolvePromise(bridgePromise, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+            self.onRejectedCallbacks.push((error) => {
+                try {
+                    let x = onRejected(error);
+                    resolvePromise(bridgePromise, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        });
+    }
+}
+MyPromise.prototype.catch = function(onRejected) {
+    return this.then(null, onRejected);
+}
+// 执行测试用例需要用到的代码
+MyPromise.deferred = function() {
+    let defer = {};
+    defer.promise = new MyPromise((resolve, reject) => {
+        defer.resolve = resolve;
+        defer.reject = reject;
+    });
+    return defer;
+}
+try {
+    module.exports = MyPromise
+} catch (e) {}
