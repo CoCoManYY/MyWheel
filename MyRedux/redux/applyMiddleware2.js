f5891d7dd7a1e220e8c87f13b7e0e086aeebca15
CoCoManYY <1344483030@qq.com>
2018-12-01 13:56:13 +0800

    支持多个中间件的

---
+++ MyRedux/redux/applyMiddleware2.js
@@ -0,0 +1,34 @@
+import compose from './compose';
+import createStore from './createStore';
+
+function applyMiddleware(...middlewares){
+    (createStore)=>(reducer)=>{
+        const store=createStore(reducer);
+        let dispatch=store.dispatch;
+        let chain=[];
+
+        const middlewareAPI={
+            getState:store.getState,
+            dispatch:(action)=>dispatch(action)
+        }
+        chain=middlewares.map(middleware=>middleware(middlewareAPI));
+        dispatch=compose(...chain)(store.dispatch);
+
+        return {
+            ...store,
+            dispatch
+        }
+    }
+}
+
+let loggerone=({dispatch,getState})=>(next)=>(action)=>{
+    console.log("loggerone:", getState());
+    next(action)
+    console.log("loggerone:", getState())
+}
+let loggertwo=({dispatch,getState})=>(next)=>(action)=>{
+    console.log("loggerone:", getState());
+    next(action)
+    console.log("loggerone:", getState())
+}
+const store=createStore(reducer,applyMiddleware([loggerone,loggertwo]))
