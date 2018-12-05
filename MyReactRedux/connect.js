a1849e75e182d5c0637cf6aa2924054f50451701
CoCoManYY <1344483030@qq.com>
2018-12-05 13:45:13 +0800

    react-redux

---
+++ MyReactRedux/connect.js
@@ -0,0 +1,32 @@
+import React,{Component} from 'react';
+import {bindActionCreators} from 'redux';
+import propTypes from 'props-types';
+export default function connnect(mapStateToProps,mapDispatchToProps){
+    return WrapedComponent=>{
+        class ProxyComponent extends Component{
+            static contextType={
+                store:propTypes.object
+            }
+            constructor(props,context){
+                super(props,context);
+                this.store=context.store;
+                this.state=mapStateToProps(this.store.getState());
+            }
+            componentWillMount(){
+                this.store.subscribe(()=>{
+                    this.setState(mapStateToProps(this.store.getState()));
+                });
+            }
+            render(){
+                let actions={};
+                if(typeof mapDispatchToProps==='function'){
+                    actions=mapDispatchToProps(this.store.dispatch);
+                }else if(typeof mapDispatchToProps==='object'){
+                    actions=bindActionCreators(mapDispatchToProps,this.store.dispatch);
+                }
+                return <WrapedComponent {...this.state} {...actions}/>
+            }
+        }
+        return ProxyComponent;
+    }
+}
