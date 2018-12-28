fd9e90a2e25dbd104bc8804eafefab4f6bff84bc
CoCoManYY <1344483030@qq.com>
2018-12-28 16:00:36 +0800

    实现Vue

---
+++ myMVVM/demo/step9/webpack.config.js
@@ -0,0 +1,19 @@
+const path=require('path');
+
+module.exports = {  
+    entry: './test.js',  //入口文件
+    output: {  //输出文件路径设置
+        path: path.resolve(__dirname, 'dist'),  
+        filename: 'app.bundle.js',  
+    },  
+    module: {       //模块的相关配置
+        rules: [     //根据文件的后缀提供一个loader,解析规则
+            {
+                test: /\.js$/,  //es6 => es5 
+                exclude: /node_modules/, 
+                // exclude:[], 不匹配选项（优先级高于test和include）
+                use: 'babel-loader'
+            }
+        ]                  
+    }
+}  
