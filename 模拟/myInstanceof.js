c0ae09fc519d6584caa16ca546627b16c719d192
CoCoManYY <1344483030@qq.com>
2019-02-11 18:55:30 +0800

    instanceod

--- 模拟/myInstanceof.js
+++ 模拟/myInstanceof.js
@@ -2,11 +2,11 @@ function myInstanceof(left,right){
     right=right.prototype;//获取构造函数的原型对象
     left=left.__proto__;//获取对象的原型对象
     while(true){
-        if(left===undefined||right===null){//左边没定义
+        if(left===right){
+            return true;
+        }else if(left===null){
             return false;
         }
-        if(right===false)
-            return true
-        right=right.__proto__;
+        left=left.__proto__;
     }
 }
