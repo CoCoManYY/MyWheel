7887be887c6ddc6c700f5a63ac8841b4bc6994f8
CoCoManYY <1344483030@qq.com>
2019-06-20 14:59:58 +0800

    emm

--- test/inherit.js
+++ test/inherit.js
@@ -49,3 +49,34 @@ function Female(name,gender,age){
 let protoType=Object.create(Person.prototype);
 protoType.constructor=Female;
 Female.prototype=protoType;
+// the best寄生组合
+function object(o) {
+    function F(){}
+    F.prototype = o;
+    return new F();
+  }
+  function inheritPrototype(subType, superType) {
+    var prototype = object(superType.prototype); // 创建对象
+    prototype.constructor = subType; // 增强对象
+    subType.prototype = prototype;  // 指定对象
+  }
+  
+  function SuperType(name) {
+    this.name = name;
+    this.colors = ['red', 'blue', 'green'];
+  }
+  SuperType.prototype.sayName = function() {
+    alert(this.name);
+  }
+  function SubType(name, age) {
+    SuperType.call(this, name);
+    this.age = age;
+  }
+  inheritPrototype(SubType, SuperType);
+  
+  SubType.prototype.sayAge = function() {
+    alert(this.age);
+  }
+  
+  var subType=new SubType('sss',111);
+  console.log(subType);
